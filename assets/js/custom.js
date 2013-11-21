ffcad800c125f474a77e78324392962cb1648300
Daniel Kastl <daniel@georepublic.de>
2013-11-22 05:24:31 +0900

    start page, contact

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -17,6 +17,68 @@ jQuery(document).ready(function() {
 			}
 		});		
 	});
+
+	$('button.subscribe').bind('click', function (evt) {
+		$.post(
+			$(this.form)[0].action, 
+			$(this.form).serialize(), 
+			function (response) {
+				if (true) {
+					switch (response) {
+						case 'Some fields are missing.':
+							alert("Some fields are missing.");
+							break;
+
+						case 'Invalid email address.':
+							alert("Invalid email address.");
+							break;
+
+						case 'Invalid list ID.':
+							alert("Invalid list ID.");
+							break;
+
+						default:
+							alert("You're subscribed!");
+							break;
+					}
+				}
+				else {
+					alert("Sorry, unable to subscribe. Please try again later!");
+				}			
+			}
+		);
+	});
+
+	$('button.message').bind('click', function (evt) {
+		$.post(
+			$(this.form)[0].action, 
+			$(this.form).serialize(), 
+			function (response) {
+				if (true) {
+					switch (response) {
+						case 'Some fields are missing.':
+							alert("Some fields are missing.");
+							break;
+
+						case 'Invalid email address.':
+							alert("Invalid email address.");
+							break;
+
+						case 'Invalid list ID.':
+							alert("Invalid list ID.");
+							break;
+
+						default:
+							alert("You're message has been sent!");
+							break;
+					}
+				}
+				else {
+					alert("Sorry, unable to send a message. Please try again later!");
+				}			
+			}
+		);
+	});
 });
 
 /**
